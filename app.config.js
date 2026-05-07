7cfa01a0057808109595e0fe8ebf1aaf61f9d02d
enes1gok <enes.gok.1752@gmail.com>
2026-05-07 23:00:47 +0300

    refactor: update app configuration and dependencies for improved structure and compatibility
    - Refactored `app.config.js` to utilize a function-based export for better configuration management.
    - Added `expo-secure-store` to the plugins in `app.json` for enhanced security features.
    - Updated versions of `expo-device`, `expo-notifications`, and `expo-secure-store` in `package.json` and `package-lock.json` for better compatibility and performance.
    
    This commit enhances the project's configuration structure and ensures that dependencies are aligned with the latest versions for improved functionality.

--- app.config.js
+++ app.config.js
@@ -1,18 +1,14 @@
-const appJson = require('./app.json');
-
 /** @type {import('@expo/config').ExpoConfig} */
-module.exports = {
-  expo: {
-    ...appJson.expo,
-    extra: {
-      ...(appJson.expo.extra ?? {}),
-      eas: {
-        ...((appJson.expo.extra && appJson.expo.extra.eas) ?? {}),
-        projectId: '51c91e75-5786-4c06-9a22-8f16e97a55a4',
-      },
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
-      deleteAccountUrl: process.env.EXPO_PUBLIC_DELETE_ACCOUNT_URL ?? '',
+module.exports = ({ config }) => ({
+  ...config,
+  extra: {
+    ...(config.extra ?? {}),
+    eas: {
+      ...((config.extra && config.extra.eas) ?? {}),
+      projectId: '51c91e75-5786-4c06-9a22-8f16e97a55a4',
     },
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+    deleteAccountUrl: process.env.EXPO_PUBLIC_DELETE_ACCOUNT_URL ?? '',
   },
-};
+});
