0ec99068bd3c823ed841179061f34bad6cf3358a
enes1gok <enes.gok.1752@gmail.com>
2026-05-07 22:46:09 +0300

    feat: configure EAS build settings and update app configuration
    - Added `eas.json` to define build and submit configurations for development, preview, and production environments.
    - Updated `app.config.js` to include a new `eas` object with the project ID for easier integration with EAS services.
    - Modified package scripts to use `expo run:android` and `expo run:ios` for improved build processes.
    
    This commit enhances the project's build configuration and streamlines the development workflow.

--- app.config.js
+++ app.config.js
@@ -6,6 +6,10 @@ module.exports = {
     ...appJson.expo,
     extra: {
       ...(appJson.expo.extra ?? {}),
+      eas: {
+        ...((appJson.expo.extra && appJson.expo.extra.eas) ?? {}),
+        projectId: '51c91e75-5786-4c06-9a22-8f16e97a55a4',
+      },
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
       deleteAccountUrl: process.env.EXPO_PUBLIC_DELETE_ACCOUNT_URL ?? '',
